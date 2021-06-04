2dd155b720c78b0a96d9be743c1ca32ffefcee68
apolopena <penavilla@gmail.com>
2021-06-04 18:54:25 +0000

    Implement TailwindCSS

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,8 +15,10 @@
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-console.log('Hello World from Webpacker')
+console.log('Webpacker be packin\'')
 // Support component names relative to this directory:
 var componentRequireContext = require.context("components", true);
 var ReactRailsUJS = require("react_ujs");
 ReactRailsUJS.useContext(componentRequireContext);
+// For TailwindCSS
+import "stylesheets/application"

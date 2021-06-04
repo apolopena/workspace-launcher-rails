172a6f39a9bfaeeb47d86d1e85c70a50e7fc04c1
apolopena <penavilla@gmail.com>
2021-06-04 22:38:07 +0000

    fix prod compile not minifying

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,5 +1,8 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const environment = require('./environment')
+const config = environment.toWebpackConfig();
+
+config.mode = 'production';
 
 module.exports = environment.toWebpackConfig()

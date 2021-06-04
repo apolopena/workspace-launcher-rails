8940b60e7cd20d92904e1abec81f2dcbb06d90cb
apolopena <penavilla@gmail.com>
2021-06-04 23:36:37 +0000

    add animate.css

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -5,7 +5,7 @@ class App extends React.Component {
     return (
       <React.Fragment>
         <div class="flex items-center justify-center h-screen">
-          <div class="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
+          <div class="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10 animate__animated animate__bounce">
             React App says: {this.props.greeting}
           </div>
         </div>

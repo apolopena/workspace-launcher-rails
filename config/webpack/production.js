9fb6022b1650e55543929c276857a8d769054f67
apolopena <penavilla@gmail.com>
2021-06-04 23:26:08 +0000

    allow for minifed production code only

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,8 +1,10 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const environment = require('./environment')
-const config = environment.toWebpackConfig();
 
+// BEGIN: force minified code (shrinks 6mb to 50k)
+const config = environment.toWebpackConfig()
 config.mode = 'production';
+// END: force minified code (shrinks 6mb to 50k)
 
 module.exports = environment.toWebpackConfig()
